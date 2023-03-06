d149b276875334b7c0fb9b4d092c80959341eb9c
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-06 11:20:02 +0100

    bonus reworked

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -1,4 +1,3 @@
-const gridDom = document.querySelector(".containerSquares");
 let gameStart = document.getElementById("avvioGioco");
 
 gameStart.addEventListener("click",
@@ -6,46 +5,66 @@ gameStart.addEventListener("click",
         const currentElement = document.querySelector(".mainBottom");
         currentElement.classList.toggle("containerList");
         
-        quadratiDaMettere();
+        createNewSquare();
     }
 );
 
-let numeroMax = 0;
-numeroMax = quadratiDaMettere();
-
-console.log(numeroMax);
-
-for (let i = 1; i < numeroMax; i++) {
-    
-
-    let currentSquare = createNewSquare(i);
-
-    currentSquare.addEventListener('click', function() {
-        currentSquare.classList.toggle('clicked');
-        currentSquare.classList.remove("d-flex");
-        console.log("la cella selezionata è la seguente: " + i);
-    });
-
-    gridDom.append(currentSquare);
-};
 
 
+function createNewSquare() {
 
-function createNewSquare(numero) {
-    const currentElement = document.createElement("div");
+    const levelDom = document.getElementById("difficolta");
+    const level = levelDom.value;
 
-    if (numeroMax == 101) {
-        currentElement.classList.add("squares");
-    } else if (numeroMax == 82){
-        currentElement.classList.add("squaresMedium");
+    console.log("numero caselle" + level);
+
+    let cells;
+    let cellForSide;
+
+    if (level == 101) {
+        cells = 100;
+    } else if (level == 82){
+        cells = 81;
     } else {
-        currentElement.classList.add("squaresHard");
+        cells = 49;
     }
-    currentElement.classList.add("d-flex");
-    currentElement.innerHTML = `${numero}`;
-    return currentElement;
+
+    cellForSide = Math.sqrt(cells);
+    console.log(cells);
+    console.log(cellForSide);
+
+    quadratiDaMettere(cells, cellForSide);
+    
+    
 };
 
+function quadratiDaMettere(cells, cellForSide) {
+    const gridDom = document.querySelector(".containerSquares");
+    gridDom.innerHTML = "";
+    for (let i = 1; i <= cells; i++) {
+        console.log(i);
+        
+        let currentCell= ownCell(cellForSide, i)
+
+        currentCell.addEventListener("click", function(){
+            this.classList.toggle("clicked");
+            console.log(i);
+        });
+
+        gridDom.append(currentCell);
+    }
+};
+
+function ownCell(cellForSide, number) {
+    const totCells = document.createElement("div");
+    totCells.classList.add("squares");
+    totCells.style.width = `calc(100% / ${cellForSide})`;
+    totCells.style.height = `calc(100% / ${cellForSide})`;
+    totCells.innerHTML = ` <div class="cell-numer">${number}</div>`;
+    return totCells;
+}
+
+/*
 function quadratiDaMettere() {
     let quadratiValue ="";
     const quadratiScelti = document.getElementById("difficolta");
@@ -53,3 +72,4 @@ function quadratiDaMettere() {
     quadratiValue = quadratiScelti.value;
     return quadratiValue;
 }
+*/
